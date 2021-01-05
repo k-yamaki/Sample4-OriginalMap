078f83662ac850c313f71d9041dc60f93d14da80
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-05 14:37:47 +0900

    Initial Commit

---
+++ Sample4-OriginalMap/Sample4_OriginalMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample4_OriginalMapApp.swift
+//  Sample4-OriginalMap
+//
+//  Created by keiji yamaki on 2021/01/05.
+//
+
+import SwiftUI
+
+@main
+struct Sample4_OriginalMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
